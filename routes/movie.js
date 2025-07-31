a404385a39c006bbfc32503def7b893b6cc2322c
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-31 18:04:27 +0200

    add movie routes for listing and retrieving movie details

--- routes/movie.js
+++ routes/movie.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get
+router.get('/', (req, res) => {
+    res.json({ message: 'List of movies' });
+});
+
+router.get('/:id', (req, res) => {
+    const movieId = req.params.id;
+    res.json({ message: `Details of movie with ID: ${movieId}` });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const connection = require('./database/connection');
+const movieRoutes = require('./routes/movie');
 
 const PORT = process.env.PORT || 3000;
 // const HOST = process.env.HOST || 'localhost';
 
 app.use(express.json());
+
+app.use('/api/movies', movieRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
